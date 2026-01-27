a21329f05bfba7b6f9ebdda462ba73f64928a828
juliopuli <49448979+juliopuli@users.noreply.github.com>
2026-01-27 15:20:18 +0100

    Actualizar script.js

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ const icons = {
 };
 
 // --- SYSTEM ASSETS (CAMUFLADO) ---
-const _graphic_assets = []; 
+const _graphic_assets = ["A54-FFF"]; 
 const _max_particle_count = 10; 
 // ---------------------------------
 
